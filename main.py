e2ab32dfde723a29ac6ab961d4f50bc564571562
Aiden Wenzel <wenzel.aiden@outlook.com>
2026-06-06 14:39:18 -0400

    use more concise imports

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from ExpenseData import ExpenseData
-from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QPushButton
 import sys
 
 class App:
     def __init__(self):
-        self.app = QtWidgets.QApplication(sys.argv)
+        self.app = QApplication(sys.argv)
 
         self.ExpenseManager = ExpenseData()
         self.ExpenseManager.load_file("experimental_original.csv")
@@ -12,20 +12,20 @@ class App:
         self.rows+=1 # need row for column names
 
         # Create table
-        self.table_widget = QtWidgets.QTableWidget(self.rows, self.cols)
+        self.table_widget = QTableWidget(self.rows, self.cols)
         self.fill_table()
 
     def fill_table(self):
         # Set headers.
         col_names = self.ExpenseManager.get_cols()
         for i in range(len(col_names)):
-            tmp_item = QtWidgets.QTableWidgetItem(col_names[i])
+            tmp_item = QTableWidgetItem(col_names[i])
             self.table_widget.setItem(0, i, tmp_item)
         
         # Set entries.
         for i in range(self.rows-1):
             for j in range(self.cols):
-                tmp_item = QtWidgets.QTableWidgetItem(str(self.ExpenseManager.data.iat[i, j]))
+                tmp_item = QTableWidgetItem(str(self.ExpenseManager.data.iat[i, j]))
                 self.table_widget.setItem(i+1, j, tmp_item)
 
 
