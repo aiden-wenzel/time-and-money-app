18d0db8c65720cc69adda33e9a59f96b1f210644
Aiden Wenzel <wenzel.aiden@outlook.com>
2026-06-16 17:56:06 -0400

    use matplot lib backend to make a chart

--- MainWidget.py
+++ MainWidget.py
@@ -1,4 +1,6 @@
 from PySide6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QPushButton, QWidget, QVBoxLayout
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT
+from matplotlib.figure import Figure
 
 class MainWidget(QWidget):
     def __init__(self, rows, cols):
@@ -15,3 +17,11 @@ class MainWidget(QWidget):
         self.layout.addWidget(self.add_button)
         self.layout.addWidget(self.save_button)
         self.layout.addWidget(self.delete_button)
+
+        # Create pie chart figure
+        # Checkout https://www.youtube.com/watch?v=AHhcwFPQlfQ for a good video on how to do this.
+        fig = Figure()
+        self.pie_canvas = FigureCanvasQTAgg(fig)
+        self.layout.addWidget(self.pie_canvas)
+        ax = fig.add_subplot()
+        ax.plot(range(10), range(10))
