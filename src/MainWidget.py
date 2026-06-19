361ff27dca8b196f22803e5fbf5f3d788005c99a
Aiden Wenzel <wenzel.aiden@outlook.com>
2026-06-19 19:41:17 -0400

    Make changes to layout

--- src/MainWidget.py
+++ src/MainWidget.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QPushButton, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QPushButton, QWidget, QGridLayout
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
 import numpy as np
@@ -13,18 +13,21 @@ class MainWidget(QWidget):
         self.save_button = QPushButton("Save")
         self.delete_button = QPushButton("Delete")
 
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.table_widget)
-        self.layout.addWidget(self.add_button)
-        self.layout.addWidget(self.save_button)
-        self.layout.addWidget(self.delete_button)
+        self.layout = QGridLayout(self)
+        self.layout.setColumnMinimumWidth(1, 600)
+        self.layout.setColumnMinimumWidth(2, 500)
+        self.layout.addWidget(self.table_widget, 1, 1)
 
         # Create pie chart figure
         # Checkout https://www.youtube.com/watch?v=AHhcwFPQlfQ for a good video on how to do this.
-        fig = Figure()
+        fig = Figure(figsize=(500, 500, "px"))
         self.pie_canvas = FigureCanvasQTAgg(fig)
-        self.layout.addWidget(self.pie_canvas)
+        self.layout.addWidget(self.pie_canvas, 1, 2)
         self.ax = fig.add_subplot()
+
+        self.layout.addWidget(self.add_button, 2, 1)
+        self.layout.addWidget(self.save_button, 3, 1)
+        self.layout.addWidget(self.delete_button, 4, 1)
     
     def fill_pie_chart(self, tag_cost_dict: dict):
 
