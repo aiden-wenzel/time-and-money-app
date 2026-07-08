b9a68f71d576789b0ed86201fc9709c6647cc4f9
Aiden Wenzel <wenzel.aiden@outlook.com>
2026-07-08 15:46:47 -0400

    test get_unique_tags

--- test/test_model.py
+++ test/test_model.py
@@ -1,11 +1,17 @@
 import os
-import Model
+from model import FinancialModel
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def test_model():
     file_path = dir_path + "/data/money_data.csv"
-    model = Model.FinancialModel(file_path)
-    assert model.get_num_rows() == 4
-    assert model.get_num_cols() == 5
+    financial_model = FinancialModel(file_path)
+    assert financial_model.get_num_rows() == 4
+    assert financial_model.get_num_cols() == 5
+
+    actual_unique_tags = financial_model.get_unique_tags()
+    expected_unique_tags = ["Groceries", "Dining"]
+    actual_unique_tags.sort() 
+    expected_unique_tags.sort()
+    assert actual_unique_tags == expected_unique_tags
